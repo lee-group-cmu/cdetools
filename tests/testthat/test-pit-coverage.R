a438eda310f2af63ddcb28ca9cd0271038d9a2fb
Rafael Izbicki <rizbicki@users.noreply.github.com>
2019-06-10 22:37:10 -0300

    change cdf to pit

--- tests/testthat/test-pit-coverage.R
+++ tests/testthat/test-pit-coverage.R
@@ -1,4 +1,4 @@
-context("cdf_coverage")
+context("pit_coverage")
 
 test_that("True density has uniform CDF coverage", {
   set.seed(12345)
@@ -7,7 +7,7 @@ test_that("True density has uniform CDF coverage", {
   z_grid <- seq(-5, 5, length.out = n_grid)
   z_test <- rnorm(n_test)
   cdes <- matrix(dnorm(z_grid), nrow = n_test, ncol = n_grid, byrow = TRUE)
-  pvals <- cdf_coverage(cdes, z_grid, z_test)
+  pvals <- pit_coverage(cdes, z_grid, z_test)
 
   suppressWarnings(test <- ks.test(pvals, "punif")) # ties due to discretization
   expect_gt(test$p.value, 0.05)
