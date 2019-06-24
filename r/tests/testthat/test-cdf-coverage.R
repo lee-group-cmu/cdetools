38e327f620d386507b8a808ce2024c32eaf5b66d
Nic Dalmasso <ndalmass@andrew.cmu.edu>
2019-06-24 17:08:58 -0400

    Changing notation from p-values -> values in code for PIT and HPD - in R. Also update their description

--- r/tests/testthat/test-cdf-coverage.R
+++ r/tests/testthat/test-cdf-coverage.R
@@ -7,8 +7,8 @@ test_that("True density has uniform CDF coverage", {
   z_grid <- seq(-5, 5, length.out = n_grid)
   z_test <- rnorm(n_test)
   cdes <- matrix(dnorm(z_grid), nrow = n_test, ncol = n_grid, byrow = TRUE)
-  pvals <- cdf_coverage(cdes, z_grid, z_test)
+  vals <- cdf_coverage(cdes, z_grid, z_test)
 
-  suppressWarnings(test <- ks.test(pvals, "punif")) # ties due to discretization
+  suppressWarnings(test <- ks.test(vals, "punif")) # ties due to discretization
   expect_gt(test$p.value, 0.05)
 })
