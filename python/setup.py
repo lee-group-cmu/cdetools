ef3f570e187917389f48f908ffbb58eccd1cc64e
Nic Dalmasso <ndalmass@andrew.cmu.edu>
2019-06-26 14:28:00 -0400

    Adding requirements and setup files for cdetools Python package

---
+++ python/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.rst", "r") as f:
+    README_TEXT = f.read()
+
+setup(
+      name="cdetools",
+      version="0.0.1",
+      license="MIT",
+      description=
+      "Tools for evaluating conditional density estimates",
+      long_description=README_TEXT,
+      author="Niccolo Dalmasso, Taylor Pospisil",
+      author_email="niccolo.dalmasso@gmail.com",
+      maintainer="niccolo.dalmasso@gmail.com",
+      url="https://github.com/tpospisi/cdetools/python",
+      classifiers=[
+                   "License :: OSI Approved :: MIT License",
+                   "Topic :: Scientific/Engineering :: Artificial Intelligence",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3.7"
+                   ],
+      keywords=["conditional density estimation", "diagnostics"],
+      package_dir={"": "src"},
+      packages=["cdetools"],
+      python_requires=">=2.7",
+      install_requires=["numpy", "scipy"],
+      setup_requires=["pytest-runner"],
+      tests_require=["pytest"],
+      zip_safe=False,
+      include_package_data=True
+      )
