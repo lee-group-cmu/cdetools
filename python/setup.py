f5e9dc32eeccd393914ac6129c4bc9f77c1a9cbe
Nic Dalmasso <ndalmass@andrew.cmu.edu>
2020-05-08 19:55:50 -0400

    Bumping the version up

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name="cdetools",
-      version="0.0.1",
+      version="0.0.2",
       license="MIT",
       description="Tools for evaluating conditional density estimates.",
       author="Niccolo Dalmasso, Taylor Pospisil",
